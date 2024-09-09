7a8caf949108b0b1f7d7432e53abad354faf7e88
JefersonT4v4res <jefersonsaj_tavares@hotmail.com>
2024-09-08 22:36:01 -0300

    :update: removendo variável e validação desnecessária

--- listarTarefas.js
+++ listarTarefas.js
@@ -1,11 +1,10 @@
-function listarTarefas(listaDeTarefas, voltarMenu = false) {
+function listarTarefas(listaDeTarefas) {
     if (listaDeTarefas.length === 0) {
       console.log('Nenhuma tarefa.');
     } else {
       console.log('Tarefas: \n');
       listaDeTarefas.forEach(tarefa => console.log(`${tarefa.id}: ${tarefa.descricao} \n`));
     }
-    if (voltarMenu) iniciar();
   }
 
 module.exports = listarTarefas;
