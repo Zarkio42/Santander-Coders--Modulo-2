5e1ec9a67a1a83756e333909043ee9e7bd9d714d
zarkio42 <pedro.contato24@gmail.com>
2024-09-09 19:03:31 -0300

    adicionando tratamento de erros

--- main.js
+++ main.js
@@ -21,31 +21,56 @@ function Main() {
         
         switch (option) {
             case '1':
-                console.log("Adicionando nova tarefa...\n");
-                const descricao = prompt("Descrição da tarefa: ");
-                adicionarTarefa(listaDeTarefas, descricao, id++);              
+                try {
+                    console.log("Adicionando nova tarefa...\n");
+                    const descricao = prompt("Descrição da tarefa: ");
+                    if (!descricao) throw new Error("Descrição não pode ser vazia.");
+                    adicionarTarefa(listaDeTarefas, descricao, id++);
+                } catch (error) {
+                    console.error("Erro ao adicionar tarefa: " + error.message);
+                }
                 break;
 
             case '2':
-                console.log("Editando tarefa...\n");
-                const idEditar = parseInt(prompt("Digite o Id da tarefa que deseja editar: "));
-                const novoConteudo = prompt("Edite o nome da tarefa: ");
-                editarTarefa(listaDeTarefas, idEditar, novoConteudo);
+                try {
+                    console.log("Editando tarefa...\n");
+                    const idEditar = parseInt(prompt("Digite o Id da tarefa que deseja editar: "));
+                    if (isNaN(idEditar) || idEditar <= 0) throw new Error("ID inválido.");
+                    const novoConteudo = prompt("Edite o nome da tarefa: ");
+                    if (!novoConteudo) throw new Error("O conteúdo da tarefa não pode ser vazio.");
+                    editarTarefa(listaDeTarefas, idEditar, novoConteudo);
+                } catch (error) {
+                    console.error("Erro ao editar tarefa: " + error.message);
+                }
                 break;
 
             case '3':
-                const idRemover = parseInt(prompt("Digite o ID da tarefa: "));
-                removerTarefa(listaDeTarefas, idRemover);
+                try {
+                    const idRemover = parseInt(prompt("Digite o ID da tarefa: "));
+                    if (isNaN(idRemover) || idRemover <= 0) throw new Error("ID inválido.");
+                    removerTarefa(listaDeTarefas, idRemover);
+                } catch (error) {
+                    console.error("Erro ao remover tarefa: " + error.message);
+                }
                 break;
 
             case '4':
-                const tarefaId = parseInt(prompt("Digite o ID da tarefa: "));
-                ObterTarefa(listaDeTarefas, tarefaId);
+                try {
+                    const tarefaId = parseInt(prompt("Digite o ID da tarefa: "));
+                    if (isNaN(tarefaId) || tarefaId <= 0) throw new Error("ID inválido.");
+                    ObterTarefa(listaDeTarefas, tarefaId);
+                } catch (error) {
+                    console.error("Erro ao obter tarefa: " + error.message);
+                }
                 break;
 
             case '5':
-                console.log("Listando todas as tarefas... \n");
-                listarTarefas(listaDeTarefas);
+                try {
+                    console.log("Listando todas as tarefas... \n");
+                    listarTarefas(listaDeTarefas);
+                } catch (error) {
+                    console.error("Erro ao listar tarefas: " + error.message);
+                }
                 break;
 
             case '6':
