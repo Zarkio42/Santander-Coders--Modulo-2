2a265b4a3a58489390af26880c837229e98ccda9
Leticia <leticiammlima.contato@gmail.com>
2024-09-09 10:32:38 -0300

    Atualização main.js - validar Id

--- main.js
+++ main.js
@@ -1,60 +1,68 @@
-const prompt = require('prompt-sync')();
-const adicionarTarefa = require('./adicionarTarefa');
-const ObterTarefa = require('./ObterTarefa');
-const listarTarefas = require('./listarTarefas');
+const prompt = require("prompt-sync")();
+const adicionarTarefa = require("./adicionarTarefa");
+const ObterTarefa = require("./ObterTarefa");
+const listarTarefas = require("./listarTarefas");
 
 let id = 1;
 let listaDeTarefas = [];
 
+function validarId(id, listaDeTarefas) {
+  if (isNaN(id) || id <= 0) {
+    console.log("ID inválido. Por favor, insira um número válido.");
+
+    return false;
+  }
+}
 function Main() {
-    console.log("----- Bem Vindo ao Agendador de Tarefas -----\n");
+  console.log("----- Bem Vindo ao Agendador de Tarefas -----\n");
 
-    const mensagemInicial = "Escolha uma opção para prosseguir: \n 1 - Adicionar Nova Tarefa \n 2 - Editar Tarefa \n 3 - Remover Tarefa \n 4 - Obter Tarefa Específica \n 5 - Listar Todas as Tarefas \n 6 - Sair";
+  const mensagemInicial =
+    "Escolha uma opção para prosseguir: \n 1 - Adicionar Nova Tarefa \n 2 - Editar Tarefa \n 3 - Remover Tarefa \n 4 - Obter Tarefa Específica \n 5 - Listar Todas as Tarefas \n 6 - Sair";
 
-    console.log(mensagemInicial);
+  console.log(mensagemInicial);
 
-    let option;
-    while (option !== '6') { 
-        option = prompt("Digite sua opção: ");
-        
-        switch (option) {
-            case '1':
-                console.log("Adicionando nova tarefa...");
-                const descricao = prompt("Descrição da tarefa: ");
-                adicionarTarefa(listaDeTarefas, descricao, id++);                
-                break;
+  let option;
+  while (option !== "6") {
+    option = prompt("Digite sua opção: ");
 
-            case '2':
-                console.log("Editando tarefa...");
-                
-                break;
+    switch (option) {
+      case "1":
+        console.log("Adicionando nova tarefa...");
+        const descricao = prompt("Descrição da tarefa: ");
+        adicionarTarefa(listaDeTarefas, descricao, id++);
+        break;
 
-            case '3':
-                console.log("Removendo tarefa...");
-                
-                break;
+      case "2":
+        console.log("Editando tarefa...");
 
-            case '4':
-                const tarefaId = prompt("Digite o ID da tarefa: ");
-                ObterTarefa(listaDeTarefas, tarefaId);
-                break;
+        break;
 
-            case '5':
-                console.log("Listando todas as tarefas... \n");
-                listarTarefas(listaDeTarefas);
-                break;
+      case "3":
+        console.log("Removendo tarefa...");
 
-            case '6':
-                console.log("Saindo.");
-                break;
+        break;
 
-            default:
-                console.log("Opção inválida, tente novamente.");
-                break;
-        }
+      case "4":
+        const tarefaId = prompt("Digite o ID da tarefa: ");
+        ObterTarefa(listaDeTarefas, tarefaId);
+        break;
 
-        console.log("\n" + mensagemInicial);
+      case "5":
+        console.log("Listando todas as tarefas... \n");
+        listarTarefas(listaDeTarefas);
+        break;
+
+      case "6":
+        console.log("Saindo.");
+        break;
+
+      default:
+        console.log("Opção inválida, tente novamente.");
+        break;
     }
+
+    console.log("\n" + mensagemInicial);
+  }
 }
 
 Main();
