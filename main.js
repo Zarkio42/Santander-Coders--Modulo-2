7c9f9c57fbfe21a71a95aca8b9c72e35442504b1
Leandro Lucas <162246108+llgsxx@users.noreply.github.com>
2024-09-18 15:42:18 -0300

    Subindo projeto

--- main.js
+++ main.js
@@ -1,51 +1,42 @@
 const prompt = require('prompt-sync')();
-const adicionarTarefa = require('./adicionarTarefa');
-const ObterTarefa = require('./ObterTarefa');
 const listarTarefas = require('./listarTarefas');
-const removerTarefa = require('./removerTarefa');
-const editarTarefa = require('./editarTarefa');
+const ObterTarefa = require('./ObterTarefa');
 
-let id = 1;
-let listaDeTarefas = [];
 
 function Main() {
     console.log("----- Bem Vindo ao Agendador de Tarefas -----\n");
 
-    const mensagemInicial = "Escolha uma opção para prosseguir: \n 1 - Adicionar Nova Tarefa \n 2 - Editar Tarefa \n 3 - Remover Tarefa \n 4 - Obter Tarefa Específica \n 5 - Listar Todas as Tarefas \n 6 - Sair \n";
+    const mensagemInicial = "Escolha uma opção para prosseguir: \n 1 - Adicionar Nova Tarefa \n 2 - Editar Tarefa \n 3 - Remover Tarefa \n 4 - Obter Tarefa Específica \n 5 - Listar Todas as Tarefas \n 6 - Sair";
 
     console.log(mensagemInicial);
 
     let option;
-    while (option !== '6') { 
+    while (option !== '6') {
         option = prompt("Digite sua opção: ");
         
         switch (option) {
             case '1':
-                console.log("Adicionando nova tarefa...\n");
-                const descricao = prompt("Descrição da tarefa: ");
-                adicionarTarefa(listaDeTarefas, descricao, id++);              
+                console.log("Adicionando nova tarefa...");
+                adicionarTarefa()
                 break;
 
             case '2':
-                console.log("Editando tarefa...\n");
-                const idEditar = parseInt(prompt("Digite o Id da tarefa que deseja editar: "));
-                const novoConteudo = prompt("Edite o nome da tarefa: ");
-                editarTarefa(listaDeTarefas, idEditar, novoConteudo);
+                console.log("Editando tarefa...");
+                editarTarefa()
                 break;
 
             case '3':
-                const idRemover = parseInt(prompt("Digite o ID da tarefa: "));
-                removerTarefa(listaDeTarefas, idRemover);
+                console.log("Removendo tarefa...");
+                removerTarefa()
                 break;
 
             case '4':
-                const tarefaId = parseInt(prompt("Digite o ID da tarefa: "));
-                ObterTarefa(listaDeTarefas, tarefaId);
+                ObterTarefa();
                 break;
 
             case '5':
-                console.log("Listando todas as tarefas... \n");
-                listarTarefas(listaDeTarefas);
+                console.log("Listando todas as tarefas...");
+                listarTarefas()
                 break;
 
             case '6':
